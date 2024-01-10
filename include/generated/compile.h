dc3f547118c9336bd1d808936e51ce59af0b486d
Youras65 <progamemer65@gmail.com>
2024-01-10 14:33:08 +0900

    First

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 19 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#19 SMP PREEMPT Sun Jan 7 22:58:24 KST 2024"
+#define LINUX_COMPILE_BY "youras65"
+#define LINUX_COMPILE_HOST "youras65-laptp"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.18.14-Dazz_kernel"
